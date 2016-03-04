f65cf9a332c7a23cc725479c4cd09623f47b0b5d
Oville <oliver195@live.se>
2016-03-04 10:14:27 +0100

    Assignment 2 and 7 done

--- Assignment_2/src/se/mah/ke/k3lara/CalculatorGui.java
+++ Assignment_2/src/se/mah/ke/k3lara/CalculatorGui.java
@@ -8,16 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class CalculatorGui extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	Calculator myCalculator;
-	private JTextField textField_1;
-	/**
-	 * Launch the application.
-	 */
+	private JTextField resultat;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -31,9 +30,6 @@ public class CalculatorGui extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public CalculatorGui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -44,53 +40,74 @@ public class CalculatorGui extends JFrame {
 		myCalculator = new Calculator();
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
 
-		JButton button = new JButton("3");
-		button.addActionListener(new ActionListener() {
+		JButton button_5 = new JButton("5");
+		button_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("3");
-				myCalculator.numberButtonPressed(3);
+				resultat.setText("5");
+				myCalculator.numberButtonPressed(5);
 			}
 		});
-		panel.add(button);
-		
-		JButton button_1 = new JButton("+");
-		button_1.addActionListener(new ActionListener() {
+		button_5.setBounds(10, 11, 89, 23);
+		panel.add(button_5);
+
+		JButton plus = new JButton("+");
+		plus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("+");
-				myCalculator.numberButtonPressed(+);
+				resultat.setText("+");
+				myCalculator.plus();
 			}
 		});
-		panel.add(button_1);
-		
-		JButton button_2 = new JButton("7");
-		button_1.addActionListener(new ActionListener() {
+		plus.setBounds(112, 11, 89, 23);
+		panel.add(plus);
+
+		JButton button8 = new JButton("8");
+		button8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textField.setText("7");
-				myCalculator.numberButtonPressed(7);
+				resultat.setText("8");
+				myCalculator.numberButtonPressed(8);
 			}
 		});
-		panel.add(button_2);
-		
-		JButton button_3 = new JButton("=");
-		btnNewButton_2.addActionListener(new ActionListener() {
+
+		button8.setBounds(211, 11, 89, 23);
+		panel.add(button8);
+
+		JButton equals = new JButton("=");
+		equals.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				myCalculator.equals();
 				int i = myCalculator.getResult();
-				textField.setText(String.valueOf(i));
+				resultat.setText(String.valueOf(i));
 			}
 		});
-		panel.add(button_3);
-		
-		textField_1 = new JTextField();
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-	
-		
 
-	
-	
-	
+		equals.setBounds(304, 11, 89, 23);
+		panel.add(equals);
+
+		resultat = new JTextField();
+		resultat.setBounds(67, 72, 270, 130);
+		panel.add(resultat);
+		resultat.setColumns(10);
+		
+		JButton button = new JButton("*");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				resultat.setText("*");
+				myCalculator.mult();
+			}
+		});
+		button.setBounds(10, 38, 89, 23);
+		panel.add(button);
+		
+		JButton button_1 = new JButton("-");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				resultat.setText("-");
+				myCalculator.minus();
+			}
+		});
+		button_1.setBounds(112, 38, 89, 23);
+		panel.add(button_1);
 	}
-
 }
--- Assignment_2/src/se/mah/ke/k3lara/HouseGui.java
+++ Assignment_2/src/se/mah/ke/k3lara/HouseGui.java
@@ -3,25 +3,20 @@ package se.mah.ke.k3lara;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class HouseGui extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextArea txtr;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -35,51 +30,39 @@ public class HouseGui extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public HouseGui() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
+		getContentPane().setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		panel.add(scrollPane);
+		scrollPane.setBounds(0, 0, 434, 261);
+		getContentPane().add(scrollPane);
+		
 		JTextArea textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
-		//1.2
+
+		// 1.2
 		House house1;
 		House house2;
 		House house3;
 		house1 = new House(1862, 342);
 		house2 = new House(2000, 138);
 		house3 = new House(1943, 98);
-		
-		
+
 		int houseSize = house1.getSize();
 		int houseYear = house1.getYearBuilt();
 		int house2Size = house2.getSize();
 		int house2Year = house2.getYearBuilt();
 		int house3Size = house3.getSize();
 		int house3Year = house3.getYearBuilt();
-		
-		System.out.println("Storleken på huset" + houseSize);
-		System.out.println("Hus 1 är byggt" + houseYear);
-        System.out.println("Storleken på huset" + house2Size);
-		System.out.println("Hus 2 är byggt" + house2Year);
 
-		System.out.println("Storleken på hus 2" + house3Size);
-		System.out.println("Hus 3 är byggt" + house3Year);
-		
-		textArea.append("Hus 1 är byggt" + houseYear + "och är" + houseSize + "kvm stort" + "\n");
-		textArea.append("Hus 2 är byggt" + house2Year + "och är" + house2Size + "kvm stort" + "\n");
-		textArea.append("Hus 3 är byggt" + house3Year + "och är" + house3Size + "kvm stort" + "\n");
-		//1.3
+		System.out.println("Storleken på huse 1 är" + " " + houseSize);
+		System.out.println("Hus 1 är byggt" + " " + houseYear);
+		System.out.println("Storleken på hus 2" + " " + house2Size);
+		System.out.println("Hus 2 är byggt" + " " + house2Year);
+
+		System.out.println("Storleken på hus 3" + house3Size);
+		System.out.println("Hus 3 är byggt" + " " + house3Year);
+		// 1.3
 		House[] myHouses = new House[10];
 		myHouses[0] = new House(1822, 131);
 		myHouses[1] = new House(1999, 193);
@@ -91,29 +74,29 @@ public class HouseGui extends JFrame {
 		myHouses[7] = new House(1911, 364);
 		myHouses[8] = new House(1801, 99);
 		myHouses[9] = new House(2012, 120);
-		
-		for(int i = 0; i < myHouses.length; i++){
-			textArea.append("Huset är byggt" + myHouses[i].getYearBuilt() + " och är" + myHouses[i].getSize() + " kvm stort" + "\n");
+
+		for (int i = 0; i < myHouses.length; i++) {
+			textArea.setText("Huset är byggt" + " " + myHouses[i].getYearBuilt() + " och är" + " "
+					+ myHouses[i].getSize() + " kvm stort" + "\n");
 		}
-		//1,4
-		ArrayList<House> myArea;
-		myArea = new ArrayList<House>();
+		// 1,4
+		ArrayList<House> myHouse;
+		myHouse = new ArrayList<House>();
 		int year;
 		int size;
-		Random rand = new Random();
-		size = rand.nextInt((1000 - 10)+ 1) + 10;
-		year = rand.nextInt((2015 - 1800)+ 1) + 1800;
-		for(int i = 0; i < 100; i++){
-			year = rand.nextInt((2015 - 1800)+ 1) + 1800;
-			size = rand.nextInt((1000 - 10)+ 1) + 10;
-			myArea.add(new House(year, size));
+		Random r = new Random();
+		size = r.nextInt((1000 - 10) + 1) + 10;
+		year = r.nextInt((2015 - 1800) + 1) + 1800;
+		for (int i = 0; i < 100; i++) {
+			year = r.nextInt((2015 - 1800) + 1) + 1800;
+			size = r.nextInt((1000 - 10) + 1) + 10;
+			myHouse.add(new House(year, size));
 		}
-		for(House house : myArea){
+		for (House house : myHouses) {
 			house.getYearBuilt();
 			house.getSize();
-			textArea.append("Huset är byggt" + String.valueOf(house.getYearBuilt()) + " och är " + String.valueOf(house.getSize()) + " kvm stort" + "\n");
+			textArea.append("Huset är byggt" + " " + String.valueOf(house.getYearBuilt()) + " och är "
+					+ String.valueOf(house.getSize()) + " kvm stort" + "\n");
 		}
 	}
 }
-	
-
